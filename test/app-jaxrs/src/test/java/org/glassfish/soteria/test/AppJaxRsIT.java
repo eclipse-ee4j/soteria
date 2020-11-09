9d85a25e1418b1a8023311fc321dc357260b20dd
arjantijms <arjan.tijms@gmail.com>
2020-11-09 15:57:25 +0100

    Renamed automatic module and made consistent for Weld SPI.
    Updated tests since they didn't work anymore, and updated Weld SPI which
    didn't work on GF 6 anymore.
    
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- test/app-jaxrs/src/test/java/org/glassfish/soteria/test/AppJaxRsIT.java
+++ test/app-jaxrs/src/test/java/org/glassfish/soteria/test/AppJaxRsIT.java
@@ -29,79 +29,85 @@ import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class AppJaxRsIT extends ArquillianBase {
-    
+
     @Deployment(testable = false)
     public static Archive<?> createDeployment() {
-        return mavenWar();
+        try {
+            return mavenWar();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Test
     public void testAuthenticated() {
         String response = readFromServer("/rest/resource/callerName?name=reza&password=secret1");
-        
+
         assertTrue(
             "Should be authenticated as user reza but was not",
             response.contains("reza"));
     }
-    
+
     @Test
     public void testNotAuthenticated() {
         String response = readFromServer("/rest/resource/callerName");
-        
+
         assertFalse(
             "Should not be authenticated as user reza but was",
             response.contains("reza"));
     }
-    
+
     @Test
     public void testHasRoleFoo() {
         String response = readFromServer("/rest/resource/hasRoleFoo?name=reza&password=secret1");
-        
+
         assertTrue(
             "Should be in role foo, but was not",
             response.contains("true"));
     }
-    
+
     @Test
     public void testNotHasRoleFoo() {
         String response = readFromServer("/rest/resource/hasRoleFoo");
-        
+
         assertTrue(
             "Should not be in role foo, but was",
             response.contains("false"));
     }
-    
+
     @Test
     public void testNotHasRoleKaz1() {
         String response = readFromServer("/rest/resource/hasRoleKaz?name=reza&password=secret1");
-        
+
         assertFalse(
             "Should not be in role kaz, but was",
             response.contains("true"));
     }
-    
+
     @Test
     public void testNotHasRoleKaz2() {
         String response = readFromServer("/rest/resource/hasRoleKaz");
-        
+
         assertFalse(
             "Should not be in role kaz, but was",
             response.contains("true"));
     }
-    
+
     @Test
     public void testSayHi() {
         String response = readFromServer("/rest/protectedResource/sayHi?name=reza&password=secret1");
-        
+
         assertTrue(
             "Endpoint should have been called, but was not",
             response.contains("saying hi!"));
     }
-    
+
     @Test
     public void testNotSayHi() {
         String response = readFromServer("/rest/protectedResource/sayHi");
-        
+
         assertFalse(
             "Endpoint should not have been called, but was",
             response.contains("saying hi!"));
