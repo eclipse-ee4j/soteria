b923b578a4aa420c87d2f9377cf7029cad82a268
Rudy De Busscher <rdebusscher@gmail.com>
2021-10-09 19:58:02 +0200

    Simple default Integration test (with integrated OpenIdConnect fake implementation)

---
+++ test/app-openid/src/test/java/org/glassfish/soteria/test/OpenIdDefaultIT.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * Contributors:
+ *   2021 : Payara Foundation and/or its affiliates
+ */
+package org.glassfish.soteria.test;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import java.io.IOException;
+import java.net.URL;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author Gaurav Gupta
+ * @author Rudy De Busscher
+ */
+
+@RunWith(Arquillian.class)
+public class OpenIdDefaultIT {
+
+    private WebClient webClient;
+
+    @OperateOnDeployment("openid-client")
+    @ArquillianResource
+    private URL base;
+
+    @Before
+    public void init() {
+        webClient = new WebClient();
+    }
+
+    @Deployment(name = "openid-server", testable = false)
+    public static Archive<?> createServerDeployment() {
+        return OpenIdTestUtil.createServerDeployment();
+    }
+
+    @Deployment(name = "openid-client", testable=false)
+    public static Archive<?> createClientDeployment() {
+        WebArchive war = OpenIdTestUtil.createClientDefaultDeployment();
+        return war;
+    }
+
+    @Test
+    @RunAsClient
+    public void testOpenIdConnect() throws IOException {
+        OpenIdTestUtil.testOpenIdConnect(webClient, base);
+    }
+
+}
