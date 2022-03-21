592d27cd75550e629574cdc8f91a2bdb05331e4c
Arjan Tijms <arjan.tijms@gmail.com>
2022-03-21 20:37:04 +0100

    Add default method to storage controller for no-timeout

--- impl/src/main/java/org/glassfish/soteria/servlet/HttpStorageController.java
+++ impl/src/main/java/org/glassfish/soteria/servlet/HttpStorageController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ * Copyright (c) 2021, 2022 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -49,6 +49,10 @@ public interface HttpStorageController {
         }
 
         return controller;
+    }
+
+    default HttpStorageController store(String name, String value) {
+        return store(name, value, null);
     }
 
     HttpStorageController store(String name, String value, Integer maxAge);
